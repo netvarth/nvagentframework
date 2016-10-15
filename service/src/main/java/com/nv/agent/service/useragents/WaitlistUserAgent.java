f6f52c03fa5b0992e4a2ffa6ed9aa1e2832d9caa
Asha <asha.chandran@netvarth.com>
2016-10-15 10:42:02 +0530

    dfgdfg

--- service/src/main/java/com/nv/agent/service/useragents/WaitlistUserAgent.java
+++ service/src/main/java/com/nv/agent/service/useragents/WaitlistUserAgent.java
@@ -27,8 +27,8 @@ public class WaitlistUserAgent extends QuartzJobBean{
 
 	private WriteDao writeDao;
 	private static final String truncate_waitlist_cache= "truncate table wl_cache_tbl";
-	private static final String update_waitlist_time= "update acct_wl_stats_tbl set waiting_time=0,waiting_updated_time='00:00:00',wl_recalculated_time='00:00:00',delay_time=0,delay_updated_time='00:00:00'";
-	private static final String update_token= "update sequence_generator_tbl set current_val=0 where table_name='WaitlistCacheEntity'";
+	private static final String update_waitlist_time= "update acct_wl_stats_tbl set acct_waiting_time=0,queue_size=0,waiting_updated_time='00:00:00',wl_recalculated_time='00:00:00',delay_time=0,delay_updated_time='00:00:00'";
+	private static final String update_token= "update sequence_generator_tbl set current_val=0 where table_name='WaitlistEntity'";
 	
 	/**
 	 * Execute job
@@ -40,8 +40,11 @@ public class WaitlistUserAgent extends QuartzJobBean{
 		logger.info("WaitlistManagerAgent executing at "+new Date());
 		try {
 			writeDao.executeUpdate(truncate_waitlist_cache);
+			logger.info("WaitlistManagerAgent waitlist_cache executing at "+new Date());
 			writeDao.executeUpdate(update_waitlist_time);
+			logger.info("WaitlistManagerAgent waitlist_time executing at "+new Date());
 			writeDao.executeUpdate(update_token);
+			logger.info("WaitlistManagerAgent update_token executing at "+new Date());
 		} catch (PersistenceException e) {
 			logger.error(new NVLogFormatter("Error while removing waitlist,delay,adjusting token and waiting time", e));
 		}
