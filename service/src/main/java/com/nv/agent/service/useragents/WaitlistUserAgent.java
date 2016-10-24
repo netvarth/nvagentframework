6ed84efbb1f061f5a717d229b659feeaea4fb9be
Asha <asha.chandran@netvarth.com>
2016-10-24 12:13:48 +0530

    sadsd

--- service/src/main/java/com/nv/agent/service/useragents/WaitlistUserAgent.java
+++ service/src/main/java/com/nv/agent/service/useragents/WaitlistUserAgent.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.nv.platform.base.dao.PersistenceException;
 import com.nv.platform.base.dao.WriteDao;
+import com.nv.platform.base.util.DateUtil;
 import com.nv.platform.log.api.NVLogFormatter;
 import com.nv.platform.log.api.NVLogger;
 import com.nv.platform.log.impl.NVLoggerAPIFactory;
@@ -27,7 +28,7 @@ public class WaitlistUserAgent extends QuartzJobBean{
 
 	private WriteDao writeDao;
 	private static final String truncate_waitlist_cache= "truncate table wl_cache_tbl";
-	private static final String update_waitlist_time= "update acct_wl_stats_tbl set acct_waiting_time=0,queue_size=0,waiting_updated_time='00:00:00',wl_recalculated_time='00:00:00',delay_time=0,delay_updated_time='00:00:00'";
+	private static final String update_waitlist_time= "update acct_wl_stats_tbl set acct_waiting_time=0,queue_size=0,waiting_updated_time=:param1,wl_recalculated_time=:param2,delay_time=0,delay_updated_time=:param3";
 	private static final String update_token= "update sequence_generator_tbl set current_val=0 where table_name='WaitlistCacheEntity'";
 	
 	/**
@@ -41,7 +42,7 @@ public class WaitlistUserAgent extends QuartzJobBean{
 		try {
 			writeDao.executeUpdate(truncate_waitlist_cache);
 			logger.info("WaitlistManagerAgent waitlist_cache executing at "+new Date());
-			writeDao.executeUpdate(update_waitlist_time);
+			writeDao.executeUpdate(update_waitlist_time,DateUtil.getNextDateWithoutTime(),DateUtil.getNextDateWithoutTime(),DateUtil.getNextDateWithoutTime());
 			logger.info("WaitlistManagerAgent waitlist_time executing at "+new Date());
 			writeDao.executeUpdate(update_token);
 			logger.info("WaitlistManagerAgent update_token executing at "+new Date());
